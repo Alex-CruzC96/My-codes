6aaa03ccbf996a08201737882a75c65ea5d7cf93
Alejandro Cruz <alexcruz_c96@outlook.com>
2023-10-09 09:42:48 -0600

    Se definió el bloqueo de las casillas principales,se programó las condiciones para el siguiente tiro

--- gatoFractal.py
+++ gatoFractal.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import messagebox
 
 root=tk.Tk()
 root.geometry("690x690")
@@ -353,16 +354,32 @@ labels=[
 
 xActiva = False 
 def principalEvent(event,rowOne,columnOne,row,column):
-    #agregar condición para que coloque X u O cada vez que este evento sea accionado 
     global xActiva
-    if (xActiva!=True):
-        labels[rowOne][columnOne][row][column].config(text="X")      
-        xActiva=True  
-    else:
-        labels[rowOne][columnOne][row][column].config(text="O")
-        xActiva=False
+    if(labels[rowOne][columnOne][row][column].cget("text")=="" and cat[rowOne][columnOne].cget("bg")!="red"):
+        if (xActiva!=True):
+            labels[rowOne][columnOne][row][column].config(text="X")      
+            xActiva=True  
+            blockingEvent(rowOne,columnOne,row,column)
+        else:
+            labels[rowOne][columnOne][row][column].config(text="O")
+            xActiva=False
+            blockingEvent(rowOne,columnOne,row,column)
     
-
+def blockingEvent(rowOne,columnOne,row,column):
+    for rowOne in range(3):
+        for columnOne in range(3):
+            if(cat[rowOne][columnOne]!=cat[row][column]):
+                cat[rowOne][columnOne].config(bg="red")
+                for r in range(3):
+                    for c in range(3):
+                        littleCats[rowOne][columnOne][r][c].config(bg="red")
+                        labels[rowOne][columnOne][r][c].config(bg="red")
+            else:
+                cat[rowOne][columnOne].config(bg="purple")
+                for r in range(3):
+                    for c in range(3):
+                        littleCats[rowOne][columnOne][r][c].config(bg="#B7B7B7")
+                        labels[rowOne][columnOne][r][c].config(bg="#B7B7B7")
 
 for row in range(3):
     for column in range(3):
@@ -375,6 +392,5 @@ for rowOne in range(3):
                 littleCats[rowOne][columnOne][row][column].grid(row=row,column=column)
                 littleCats[rowOne][columnOne][row][column].bind("<Button-1>",lambda event,eve=littleCats[rowOne][columnOne][row][column],one=rowOne,two=columnOne,three=row,four=column:principalEvent(eve,one,two,three,four))
                 labels[rowOne][columnOne][row][column].place(x=19,y=10)
-
 
 root.mainloop()
